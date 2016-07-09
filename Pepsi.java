d1332c37b90efb9a508c5ffbfb8f2be4cf60172b
Amandeep Sarow <asarow@ucsd.edu>
2016-07-09 04:28:58 -0700

    Implemented static factory methods

--- Pepsi.java
+++ Pepsi.java
@@ -1,3 +1,19 @@
 public class Pepsi implements Soda {
+    private final boolean containsSugar;
     
+    private Pepsi(boolean containsSugar) {
+	this.containsSugar = containsSugar;
+    }
+
+    public static Pepsi withRealSugar() {
+	return new Pepsi(true);
+    }
+
+    public static Pepsi withHighFructoseCornSyrup() {
+	return new Pepsi(false);
+    }
+
+    public boolean isRealSugar() {
+	return containsSugar;
+    }
 }
